b488bb0e1f1f129933b70c4e8e40d6225537fd5f
rsotomay <138070004+rsotomay@users.noreply.github.com>
2023-08-04 21:53:42 -0500

    adds NFT testing

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,10 +5,17 @@ import './ERC721Enumerable.sol';
 import './Ownable.sol';
 
 contract NFT is ERC721Enumerable, Ownable {
+    //allows converting uints to string with .toString()
+    using Strings for uint256;
+
+    string public baseURI;
+    string public baseExtension = ".json";
     uint256 public cost;
     uint256 public maxSupply;
     uint256 public allowMintingOn;
-    string public baseURI;
+
+    event Mint(uint256 amount, address minter);
+    event Withdraw(uint256 amount, address owner);
 
     constructor (
         string memory _name, 
@@ -23,5 +30,62 @@ contract NFT is ERC721Enumerable, Ownable {
             allowMintingOn = _allowMintingOn;
             baseURI = _baseURI;
         } 
+
+        function mint(uint256 _mintAmount) public payable {
+            //Only allow minting after specified time
+            require(block.timestamp >= allowMintingOn);
+            //Must mint at least one nft
+            require(_mintAmount > 0, 'Must mint minimum 1 nft');
+            //Must provide the correct amount of eth to mint
+            require(msg.value >= cost * _mintAmount, "Not enough funds provided");
+            //Takes totalSupply from enumerable and stores it in supply
+            uint256 supply = totalSupply();
+            //Prevent them from minting more tokens than available
+            require(supply + _mintAmount <= maxSupply,'Minting amount not allowed');
+            //Create a NFT
+            // Loop to allow multiple minting
+            for(uint256 i = 1; i <= _mintAmount; i++) {
+            // Calls safeMint from ERC721(impored in eumerable) and passes the owner 
+            //and add 1 to supply as token id
+            _safeMint(msg.sender, supply + i);
+            }
+            emit Mint(_mintAmount, msg.sender);
+        }
+
+        //Returns metadata IPFS url
+        //EG: 'ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/1.json'
+        function tokenURI(uint256 _tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+        {
+            require(_exists(_tokenId), 'token does not exists');
+            return string(abi.encodePacked(baseURI, _tokenId.toString(), baseExtension));
+        }
+
+        //Returns all NFTs owned by an owner by token Ids
+        function walletOfOwner(address _owner) public view returns(uint256[] memory) {
+            uint256 ownerTokenCount = balanceOf(_owner);
+            uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+            for(uint256 i; i < ownerTokenCount; i++) {
+                tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+            }
+            return tokenIds;
+        }
+
+        function withdraw() public onlyOwner {
+            uint256 balance = address(this).balance;
+
+            (bool success, ) = payable (msg.sender).call{value: balance}('');
+            require(success);
+
+            emit Withdraw(balance ,msg.sender);
+        }
+
+        function setCost(uint256 _newCost) public onlyOwner {
+            cost = _newCost;
+        }
 
 }
